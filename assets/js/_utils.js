7b261220eb05a3f92639e6b950654d387a1fd5aa
Victor Inknov <victorhonly@gmail.com>
2022-10-06 20:12:01 -0300

    adicionando banner

--- assets/js/_utils.js
+++ assets/js/_utils.js
@@ -1,2 +1,9 @@
 let allImages = document.querySelectorAll('img')
 allImages.forEach(e => e.setAttribute('draggable', 'false'))
+
+
+let header = document.querySelector('header')
+let logo = document.getElementById('logo')
+document.body.onscroll = () => {
+    window.scrollY == '0' ? header.classList.remove('header-bg') : header.classList.add('header-bg')
+}
